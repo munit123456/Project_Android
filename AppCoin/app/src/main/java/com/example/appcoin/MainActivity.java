a805a04a0d77c9057de1615f942f3f8dcb0acb1d
DAT <imdatdeptrai@gmail.com>
2022-10-05 00:19:44 +0700

    design login

--- AppCoin/app/src/main/java/com/example/appcoin/MainActivity.java
+++ AppCoin/app/src/main/java/com/example/appcoin/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.appcoin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn_login, btn_sign;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_login = (Button) findViewById(R.id.button_Login);
+        btn_sign = (Button) findViewById(R.id.button_SIGN);
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+        btn_sign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SignActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
